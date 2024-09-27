d1e44ff8658cc31dd48fe5b1ca4140db3757cb3a
CannonTheGoat <cannon.robinson@hgs.hiddengeniusproject.org>
2024-09-26 17:11:18 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,17 @@
 function darkMode() {
-// Your code here.
-}
+
+    // Get the body element
+    const body = document.body;
+
+    // Write the code to toggle dark mode below, HINT use the style proporty of the body element
+    if (body.style.backgroundColor === 'black') {
+        // If its black, change to light mode
+        body.style.backgroundColor = 'white';
+       body.style.color = 'black';
+    } else {
+        // If its not black, change to dark mode
+        body.style.backgroundColor = 'black'
+        body.style.color = 'black';
+    }
+
+  }
